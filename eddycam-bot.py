2af6f80f97e747236a5462e2c96d59b326239c73
Edward Zhang <edward@zhang.nz>
2022-05-28 21:06:15 +1000

    Limited allowed chats

--- eddycam-bot.py
+++ eddycam-bot.py
@@ -14,14 +14,23 @@ with open('imageurl.txt') as f:
 with open('key.txt') as f:
     token = f.readline().strip()
 
+with open('allowedchatid.txt') as f:
+    allowed_chat = int(f.readline().strip())
+
 async def neko(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    print(update.effective_chat.id)
     await context.bot.send_message(chat_id=update.effective_chat.id, text="nyaa~")
 
 async def snapshot(update: Update, context: CallbackContext.DEFAULT_TYPE):
     image_request = requests.get(imageurl, stream=True)
     image_request.raw.decode_content = True
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Let me take a picture for you! Please wait...")
-    await context.bot.send_photo(update.effective_chat.id, image_request.raw, caption="here's your picture uwu")
+    if (update.effective_chat.id == allowed_chat):
+        print(update.effective_chat.id)
+        print(allowed_chat)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Sure thing, senpai! Let me take a picture now!")
+        await context.bot.send_photo(update.effective_chat.id, image_request.raw, caption="here's your picture uwu")
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="b...baka! You're not allowed to see my photos!")
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(token).build()
