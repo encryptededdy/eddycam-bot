44ac6327d38d71cff219adba47f8d6b13a659a48
Edward Zhang <edward@zhang.nz>
2022-06-29 02:49:16 +1000

    Looser criteria for full day

--- sftpcrawler.py
+++ sftpcrawler.py
@@ -20,7 +20,7 @@ def get_image(folder, localpath, id = None):
             images = conn.listdir()
         images = list(filter(lambda image: image.endswith(".jpg"), images)) # todo better sorting
         images.sort(key=lambda image: image[-9:-4]) # sort low to high
-        if (id == None and len(images) == 288): # full day
+        if (id == None and len(images) > 285): # full day
             id = 142 # midday
         elif (id == None):
             id = len(images) - 1 # last image
